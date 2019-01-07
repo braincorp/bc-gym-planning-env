83d850fb7be103f3d35a8bcb2ca977b12f7bbec5
olegsinyavskiy <olegsinyavskiy@gmail.com>
2019-01-07 12:22:55 -0800

    simple setup

---
+++ setup.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+from setuptools import setup, Extension, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='sparse_rrt',
+    version='0.0.1',
+    description='Sparse stable trees planner',
+    long_description=long_description,
+    author='Oleg Sinyavskiy',
+    author_email='olegsinyavskiy@gmail.com',
+    url='https://github.com/olegsinyavskiy/sparse_rrt',
+    download_url='',
+    license='Braincorp',
+    install_requires=['numpy>=1.13.3',
+                      'gym',
+                      'attrs',
+                      'opencv-python'],
+    package_data={'': ['input']},
+    include_package_data=True,
+    extras_require={
+        'tests': ['pytest>=2.7.2',
+                  'pytest-pep8>=1.0.6',
+                  'pytest-xdist>=1.13.1'],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=find_packages(),
+)
