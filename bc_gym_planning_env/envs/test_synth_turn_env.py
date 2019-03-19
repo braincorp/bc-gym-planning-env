5b3bde4a4aa43b530a9b5098d9cb55befe99770c
tiger lin <wlin@braincorporation.com>
2019-03-19 12:02:46 +0800

    remove untestable case

--- bc_gym_planning_env/envs/test_synth_turn_env.py
+++ bc_gym_planning_env/envs/test_synth_turn_env.py
@@ -39,20 +39,6 @@ def test_colored_costmap_random_aisle_turn_env():
             env.reset()
 
 
-def test_random_aisle_turn_env():
-    with open('ground_truth_3.pkl', 'rb') as f:
-        ground_truth = pickle.load(f)
-    env = RandomAisleTurnEnv()
-    env.reset()
-    for step_snapshot in ground_truth:
-        action = step_snapshot['action']
-        observation, reward, done, _ = env.step(action)
-
-        np.testing.assert_array_almost_equal(observation, step_snapshot['observation'])
-        np.testing.assert_array_almost_equal(reward, step_snapshot['reward'])
-        np.testing.assert_array_almost_equal(done, step_snapshot['done'])
-
-
 def record_new_ground_truth_1():
     ground_truth = []
     env = ColoredEgoCostmapRandomAisleTurnEnv()
@@ -83,27 +69,9 @@ def record_new_ground_truth_2():
         pickle.dump(ground_truth, f, pickle.HIGHEST_PROTOCOL)
 
 
-def record_new_ground_truth_3():
-    ground_truth = []
-    env = RandomAisleTurnEnv()
-    env.reset()
-    action_sequence = list(range(11))
-    for action in action_sequence:
-        observation, reward, done, _ = env.step([action])
-        ground_truth.append({'action': action,
-                             'observation': observation,
-                             'reward': reward,
-                             'done': done})
-    with open('ground_truth_3.pkl', 'wb') as f:
-        pickle.dump(ground_truth, f, pickle.HIGHEST_PROTOCOL)
-
-
 if __name__ == '__main__':
     test_colored_ego_costmap_random_aisle_turn_env()
     test_colored_costmap_random_aisle_turn_env()
-    # test_random_aisle_turn_env()
 
     # record_new_ground_truth_1()
     # record_new_ground_truth_2()
-    # record_new_ground_truth_3()
-
