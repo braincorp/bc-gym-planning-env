4fc4d5602efc3e16e980d66d3e840de81969e270
tiger lin <wlin@braincorporation.com>
2019-03-20 16:20:46 +0800

    add pure pursuit feature

--- bc_gym_planning_env/envs/synth_turn_env.py
+++ bc_gym_planning_env/envs/synth_turn_env.py
@@ -388,6 +388,7 @@ class ColoredEgoCostmapRandomAisleTurnEnv(RandomAisleTurnEnv):
         self._egomap_y_bounds = np.array([-2., 2.])  # orthogonal to robot's direction
         resulting_size = (self._egomap_x_bounds[1] - self._egomap_x_bounds[0],
                           self._egomap_y_bounds[1] - self._egomap_y_bounds[0])
+        self._path_tracking_idx = 0
 
         pixel_size = world_to_pixel(np.asarray(resulting_size, dtype=np.float64), np.zeros((2,)), resolution=0.03)
         data_shape = (pixel_size[1], pixel_size[0], 1)
@@ -395,6 +396,13 @@ class ColoredEgoCostmapRandomAisleTurnEnv(RandomAisleTurnEnv):
             ('environment', spaces.Box(low=0, high=255, shape=data_shape, dtype=np.uint8)),
             ('goal', spaces.Box(low=-1., high=1., shape=(2, 1), dtype=np.float64))
         )))
+
+    def _find_next_goal(self, ego_path, radius=2.):
+        for i in range(self._path_tracking_idx, len(ego_path)):
+            if np.linalg.norm(ego_path[i, :2]) > radius:
+                self._path_tracking_idx = i
+                return ego_path[i]
+        return ego_path[-1]  # if no valid point, return last point
 
     def _extract_egocentric_observation(self, rich_observation):
         """Extract egocentric map and path from rich observation
@@ -423,7 +431,7 @@ class ColoredEgoCostmapRandomAisleTurnEnv(RandomAisleTurnEnv):
 
         ego_path = from_global_to_egocentric(rich_observation.path, robot_pose)
         obs = np.expand_dims(ego_costmap.get_data(), -1)
-        normalized_goal = ego_path[-1, :2] / ego_costmap.world_size()
+        normalized_goal = self._find_next_goal(ego_path)[:2] / ego_costmap.world_size()
         normalized_goal = normalized_goal / np.linalg.norm(normalized_goal)
         return OrderedDict((('environment', obs),
                             ('goal', np.expand_dims(normalized_goal, -1))))
