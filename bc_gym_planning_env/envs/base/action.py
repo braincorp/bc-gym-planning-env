beaf1a443271d2e8e3bc5269e05b8d147d5cf0f4
tiger lin <wlin@braincorporation.com>
2019-03-25 10:23:50 +0800

    fix bug

--- bc_gym_planning_env/envs/base/action.py
+++ bc_gym_planning_env/envs/base/action.py
@@ -27,9 +27,8 @@ class Action(object):
         if command.shape == (2,):
             self.command = command
         elif command.shape == (1,):
-            # self.command = self._command_dict[command[0]]
-            self.command = np.array([0.2, command[0]])
-        elif command.shape == (1,1):
+            self.command = self._command_dict[command[0]]
+        elif command.shape == (1, 1):
             self.command = np.array([0.2, command[0, 0]])
         else:
             raise NotImplementedError
