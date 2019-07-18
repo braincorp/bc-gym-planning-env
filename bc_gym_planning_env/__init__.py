5e165c31676d0d371b49b5460bd98092285424ff
Alexander Khoury <akhoury727@gmail.com>
2019-07-18 16:06:00 -0700

    fix init

--- bc_gym_planning_env/__init__.py
+++ bc_gym_planning_env/__init__.py
@@ -1,6 +1,10 @@
+# pylint: disable=no-name-in-module, import-error
+"""bc_gym_planning_env.__init__.py
+Setup for importing the cpp module within bc_gym_planning_env
+"""
 from __future__ import print_function, absolute_import, division
 
-from bc_gym_planning_env._pixel_raytracing_utils import raytrace_2d, raytrace_clean_on_input_map
-from bc_gym_planning_env._pixel_raytrace_module import PixelRaytraceWrapper
+from bc_gym_planning_env._bc_gym_cpp_module.pixel_raytracing_utils import raytrace_2d, raytrace_clean_on_input_map
+from bc_gym_planning_env._bc_gym_cpp_module.pixel_raytrace_module import PixelRaytraceWrapper
 
-__all__ = ["PixelRaytraceBoostWrapper", "raytrace_2d_impl", "raytrace_clean_on_input_map_impl"]
+__all__ = ["PixelRaytraceWrapper", "raytrace_2d", "raytrace_clean_on_input_map"]
